3ff3804e7c6320f3388bf4e240ff06ea8bf2744b
gerardo4porciento-beep <gerardo4porciento-beep@users.noreply.github.com>
2025-12-27 21:03:44 -0400

    Force Vercel rebuild: Explicit outDir in vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
